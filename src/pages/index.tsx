da80e9da58ffe1a864b3990f2cd5a24277f71233
bartol <b@bartol.dev>
2019-08-11 20:13:45 +0200

    pages: index, success, failure, 404

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from '../components/search'
+import Layout from '../components/Layout'
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <Search />
+    </Layout>
+  )
+}
+
+export default App
