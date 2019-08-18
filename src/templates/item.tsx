7cdee0ba3adbdb2fd469872509fc3be18e4a6972
bartol <b@bartol.dev>
2019-08-18 21:28:56 +0200

    redux add to cart from item page

--- src/templates/item.tsx
+++ src/templates/item.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import { graphql, Link } from 'gatsby'
+import React, { useState } from 'react'
+import { graphql } from 'gatsby'
 import Image from 'gatsby-image'
 import '../styles/main.scss'
+import { connect } from 'react-redux'
+// @ts-ignore
+import { addToCart } from '../state/actions'
+import ItemImage from '../components/ItemImage'
+import Layout from '../components/Layout'
 
 interface Props {
   data: {
@@ -18,36 +23,61 @@ interface Props {
       nodes: object[]
     }
   }
+  addToCart: any
+  cart: any
 }
 
-const Item: React.FC<Props> = ({ data }) => {
+const Item: React.FC<Props> = ({ data, addToCart }) => {
   const { name, price, desc, slug, qt, avb, id__normalized: id } = data.items
   const { nodes: images } = data.allS3Image
+  const [image, setImage] = useState(0)
   return (
-    <div>
-      <p>Name: {name}</p>
-      {images.map((image: any) => {
-        // TODO inplement interface
-        const { fluid } = image.localFile.childImageSharp
-        return (
-          <div className="div">
-            <Link to={`/${slug}/`}>
-              <Image fluid={fluid} />
-            </Link>
+    <Layout>
+      <div className="itemWrapper">
+        <div className="carousel">
+          <ItemImage image={images[image]} />
+          <div className="slider">
+            {images.map((image: any, index: any) => {
+              const { localFile, Key } = image
+              const { childImageSharp } = localFile
+              const { fluid } = childImageSharp
+              return (
+                <div
+                  className="slider-image"
+                  key={Key}
+                  onClick={() => {
+                    setImage(index)
+                    console.log(index)
+                  }}
+                >
+                  <Image fluid={fluid} />
+                </div>
+              )
+              // return <pre>{JSON.stringify(image)}</pre>
+            })}
           </div>
-        )
-      })}
-      <p>Slug: {slug}</p>
-      <p>Price: {price}</p>
-      <p>Id: {id}</p>
-      <p>Description: {desc}</p>
-      <p>Quantity: {qt}</p>
-      <p>Availability: {avb}</p>
-    </div>
+        </div>
+        <div className="details">
+          <p>Name: {name}</p>
+          <p>Slug: {slug}</p>
+          <p>Price: {price / 100} kn</p>
+          <p>Id: {id}</p>
+          <p>Description: {desc}</p>
+          <p>Quantity: {qt}</p>
+          <p>Availability: {avb}</p>
+          <button type="button" onClick={() => addToCart(data.items)}>
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
-export default Item
+export default connect(
+  null,
+  { addToCart }
+)(Item)
 
 export const query = graphql`
   query($id: String!, $regex: String!) {
@@ -65,6 +95,7 @@ export const query = graphql`
       sort: { fields: Key, order: ASC }
     ) {
       nodes {
+        Key
         localFile {
           childImageSharp {
             fluid(maxWidth: 500) {
