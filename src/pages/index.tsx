89701560940e3a814da8d9126db5a14572daf737
Bartol Deak <b@bdeak.net>
2019-08-18 21:30:38 +0200

    layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Search from '../components/search'
 import Layout from '../components/Layout'
+import Cart from '../components/cart'
 
 const App: React.FC = () => {
+  const [cartOpened, isCartOpened] = useState(false)
+
   return (
     <Layout>
-      <Search />
+      <Search opened={cartOpened} />
+      <button
+        type="button"
+        className={`toggleCart ${cartOpened ? 'open' : 'close'}`}
+        onClick={() => isCartOpened(!cartOpened)}
+      >
+        Cart
+      </button>
+      <Cart opened={cartOpened} />
     </Layout>
   )
 }
