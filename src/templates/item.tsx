6cc28c286b32d0792afe72346e4bdcd02fa7f4ba
bartol <b@bartol.dev>
2019-08-25 14:40:19 +0200

    image carousel

--- src/templates/item.tsx
+++ src/templates/item.tsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { graphql } from 'gatsby'
 import Image from 'gatsby-image'
 import '../styles/main.scss'
 import { connect } from 'react-redux'
-// @ts-ignore
-import { addToCart } from '../state/actions'
-import ItemImage from '../components/ItemImage'
+import { ChevronRight, ChevronLeft } from 'react-feather'
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  Dot
+} from 'pure-react-carousel'
+import {
+  addToCart as reduxAddToCart,
+  toggleCart as reduxToggleCart
+  // @ts-ignore
+} from '../state/actions'
+import Cart from '../components/cart'
 import Layout from '../components/Layout'
+import 'pure-react-carousel/dist/react-carousel.es.css'
 
 interface Props {
   data: {
@@ -25,58 +38,87 @@ interface Props {
   }
   addToCart: any
   cart: any
+  menu: any
+  toggleCart: any
 }
 
-const Item: React.FC<Props> = ({ data, addToCart }) => {
-  const { name, price, desc, slug, qt, avb, id__normalized: id } = data.items
+const Item: React.FC<Props> = ({ data, addToCart, menu, toggleCart }) => {
+  const { name, price, desc, qt, id__normalized: id } = data.items
   const { nodes: images } = data.allS3Image
-  const [image, setImage] = useState(0)
+
   return (
     <Layout>
-      <div className="itemWrapper">
-        <div className="carousel">
-          <ItemImage image={images[image]} />
-          <div className="slider">
+      <div className={`itemWrapper ${menu.isCartOpened ? 'open' : 'close'}`}>
+        <CarouselProvider
+          naturalSlideWidth={500}
+          naturalSlideHeight={500}
+          totalSlides={images.length}
+        >
+          <div className="imageWrapper">
+            <Slider>
+              {images.map((image: any, index: any) => {
+                const { localFile, Key } = image
+                const { childImageSharp } = localFile
+                const { fluid } = childImageSharp
+                return (
+                  <Slide index={index} key={Key}>
+                    <Image fluid={fluid} />
+                  </Slide>
+                )
+              })}
+            </Slider>
+
+            <ButtonBack className="buttonBack">
+              <ChevronLeft size={48} />
+            </ButtonBack>
+            <ButtonNext className="buttonNext">
+              <ChevronRight size={48} />
+            </ButtonNext>
+          </div>
+          <div className="dotWrapper">
             {images.map((image: any, index: any) => {
               const { localFile, Key } = image
               const { childImageSharp } = localFile
-              const { fluid } = childImageSharp
+              const { fixed } = childImageSharp
               return (
-                <div
-                  className="slider-image"
-                  key={Key}
-                  onClick={() => {
-                    setImage(index)
-                    console.log(index)
-                  }}
-                >
-                  <Image fluid={fluid} />
-                </div>
+                <Dot slide={index} key={Key} className="dot">
+                  <Image fixed={fixed} />
+                </Dot>
               )
-              // return <pre>{JSON.stringify(image)}</pre>
             })}
           </div>
-        </div>
+        </CarouselProvider>
         <div className="details">
-          <p>Name: {name}</p>
-          <p>Slug: {slug}</p>
-          <p>Price: {price / 100} kn</p>
-          <p>Id: {id}</p>
-          <p>Description: {desc}</p>
-          <p>Quantity: {qt}</p>
-          <p>Availability: {avb}</p>
+          <h1>{name}</h1>
+          <i>{id}</i>
+          <h2>{price / 100} kn</h2>
+          <p>{desc}</p>
+          <p>{qt} kom.</p>
           <button type="button" onClick={() => addToCart(data.items)}>
             Add to cart
           </button>
         </div>
+        <button
+          type="button"
+          // className={`toggleCart ${cartOpened ? 'open' : 'close'}`}
+          className={`toggleCart ${menu.isCartOpened ? 'open' : 'close'}`}
+          // onClick={() => isCartOpened(!cartOpened)}
+          onClick={() => toggleCart()}
+        >
+          Cart
+        </button>
+        {/* <Cart opened={cartOpened} /> */}
+        <Cart opened={menu.isCartOpened} />
       </div>
     </Layout>
   )
 }
 
 export default connect(
-  null,
-  { addToCart }
+  (state: any) => ({
+    menu: state.menu
+  }),
+  { addToCart: reduxAddToCart, toggleCart: reduxToggleCart }
 )(Item)
 
 export const query = graphql`
@@ -100,6 +142,11 @@ export const query = graphql`
           childImageSharp {
             fluid(maxWidth: 500) {
               ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+          childImageSharp {
+            fixed(width: 75) {
+              ...GatsbyImageSharpFixed_withWebp
             }
           }
         }
