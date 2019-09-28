53a62b4ef51477fd5e2495faee42eccddfdfe9a0
Bartol Deak <b@bdeak.net>
2019-09-28 12:09:45 +0200

    delete everything

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# To-Do List
+
+- [x] version 0.1.0
+- [x] delete everything
+- [ ] typescript
+- [ ] eslint
+- [ ] sass
+- [ ] storybook
--- src/pages/failure.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default () => (
-  <div>
-    <h1>Your payment failed</h1>
-    <p>Please contact us.</p>
-  </div>
-)
