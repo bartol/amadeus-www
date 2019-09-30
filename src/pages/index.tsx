cfe73dbc15503aafd478e4feb1796603cf261a5e
Bartol Deak <b@bdeak.net>
2019-09-30 23:11:41 +0200

    wiring up new components in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,28 @@
-import { graphql, Link } from 'gatsby'
-import Image from 'gatsby-image'
+import { graphql } from 'gatsby'
 import React from 'react'
+import Card from '../components/card'
+import Layout from '../components/layout'
 import '../styles/custom.css'
 
 const index: React.FC<Props> = ({ data }) => {
   const { items } = data.amadeus
   return (
-    <div className='container mx-auto'>
-      <h1 className='bg-blue-200 text-center text-2xl'>amadeus2.hr</h1>
-      <ul>
+    <Layout>
+      <ul className='flex mb-4'>
         {items.map(item => {
           const { name, price, slug, optimizedImages } = item
           return (
-            <li key={slug}>
-              <Link to={`/${slug}/`}>
-                <h2>{name}</h2>
-              </Link>
-              <h3 className='text-5xl'>{price / 100} kn</h3>
-              {optimizedImages.map((optimizedImage, index) => {
-                return (
-                  <Image
-                    fixed={optimizedImage.childImageSharp.fixed}
-                    key={optimizedImage.childImageSharp.fixed.src}
-                    alt={`Image ${index} of ${name}`}
-                  />
-                )
-              })}
-            </li>
+            <Card
+              name={name}
+              slug={slug}
+              price={price}
+              optimizedImage={optimizedImages[0]}
+              key={slug}
+            />
           )
         })}
       </ul>
-      <button className='btn btn-blue'>Button</button>
-      <br />
-      <button className='text-3xl py-2 px-6 rounded bg-green-400 hover:bg-green-500 active:bg-green-600 trans trans-bg focus:outline-none'>
-        Kupi ovaj proizvod
-      </button>
-    </div>
+    </Layout>
   )
 }
 
