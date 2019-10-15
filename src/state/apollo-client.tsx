1c1ddd34fa0ce4ca909b51006cb752f1f7add745
Bartol Deak <b@bdeak.net>
2019-10-15 16:53:45 +0200

    feat: apollo persistent cache

--- src/state/apollo-client.tsx
+++ src/state/apollo-client.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { ApolloProvider as _ApolloProvider } from '@apollo/react-hooks'
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { persistCache } from 'apollo-cache-persist'
 import fetch from 'isomorphic-fetch'
+import { isBrowser } from '../helpers/isBrowser'
 
-export const client = new ApolloClient({
+const cache = new InMemoryCache()
+
+if (isBrowser()) {
+  persistCache({
+    cache,
+    storage: window.localStorage,
+  })
+}
+
+const client = new ApolloClient({
   uri: 'https://api.amadeus2.hr',
+  cache,
   fetch,
 })
 
