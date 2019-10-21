1857c22eb2f67149125bab5dc52e3f9d48cbbea1
bartol <b@bartol.dev>
2019-10-21 12:34:11 +0200

    refactor: card component styles

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -26,27 +26,34 @@ const Card = ({
   }
 
   return (
-    <li
-      key={slug}
-      className='w-1/4 p-3 rounded-lg border border-gray-200  mx-auto'
-    >
-      <Link
-        to={`${language === 'hr' ? '/' : `/${language}/`}${slug}/`}
-        className='w-full h-full'
-      >
-        <Image
-          fixed={optimizedImage.childImageSharp.fixed}
-          key={optimizedImage.childImageSharp.fixed.src}
-          alt={`${name} image`} // TODO seems like it isn't working
-          fadeIn
-        />
-        <h2 className='text-2xl'>{name}</h2>
-        <h3 className='text-xl'>{currencyConversion(price)}</h3>
-        <h3 className='text-xl'>{getQuantity(id, quantity)}</h3>
-      </Link>
-      <button type='button' onClick={() => addToCart(addToCartItem)}>
-        add to cart
-      </button>
+    <li key={slug} className='w-1/3 p-3 rounded-lg'>
+      <div className='w-full h-full rounded-lg border border-gray-200 overflow-hidden flex flex-col justify-between'>
+        <Link
+          to={`${language === 'hr' ? '/' : `/${language}/`}${slug}/`}
+          className='flex-grow'
+        >
+          <div className='p-3'>
+            <Image
+              fluid={optimizedImage.childImageSharp.fluid}
+              key={optimizedImage.childImageSharp.fluid.src}
+              alt={`${name} image`} // TODO seems like it isn't working
+              fadeIn
+            />
+            <h2 className='text-2xl'>{name}</h2>
+            <h3 className='text-xl'>{currencyConversion(price)}</h3>
+            <h3 className='text-xl'>{getQuantity(id, quantity)}</h3>
+          </div>
+        </Link>
+        <div className='p-3'>
+          <button
+            type='button'
+            onClick={() => addToCart(addToCartItem)}
+            className='w-full bg-gray-400 py-2 rounded'
+          >
+            add to cart
+          </button>
+        </div>
+      </div>
     </li>
   )
 }
