d4b0a787fd3078086f5c8b43c2988702d14bfa1c
bartol <b@bartol.dev>
2020-01-05 15:43:10 +0100

    Mobile banner conditional rendering with css

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'gatsby-image'
-import { isBrowser } from '../helpers/isBrowser'
 import { CarouselProvider, Dot, Slide, Slider } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
 
@@ -11,82 +10,80 @@ const Banner: React.FC = ({ banners }) => {
   } = banners
 
   return (
-    <div className='w-full'>
-      {isBrowser() && window.innerWidth}
-      {isBrowser() && window.innerWidth < 767 ? (
-        <CarouselProvider
-          naturalSlideWidth={4}
-          naturalSlideHeight={3}
-          totalSlides={mobileImages.length}
-          isPlaying
-          interval={5000}
-          infinite='true'
-        >
-          <Slider>
-            {mobileImages.map((image, index) => {
-              return (
-                <Slide index={index} key={index}>
-                  <Image
-                    fluid={image.childImageSharp.fluid}
-                    alt={`Banner ${index + 1}`}
-                    loading={index === 0 ? 'eager' : 'lazy'}
-                    className='select-none'
-                    fadeIn
-                  />
-                </Slide>
-              )
-            })}
-          </Slider>
-          <nav>
-            {mobileImages.map((image, index) => {
-              return (
-                <Dot
-                  slide={index}
-                  key={index}
-                  className='focus:outline-none focus:shadow-outline mx-1 h-3 w-3 bg-red-500 rounded-full isDisabled'
+    <>
+      <CarouselProvider
+        naturalSlideWidth={3}
+        naturalSlideHeight={1}
+        totalSlides={desktopImages.length}
+        isPlaying
+        interval={5000}
+        infinite='true'
+        className='desktopImages'
+      >
+        <Slider>
+          {desktopImages.map((image, index) => {
+            return (
+              <Slide index={index} key={index}>
+                <Image
+                  fluid={image.childImageSharp.fluid}
+                  alt={`Banner ${index + 1}`}
+                  loading={index === 0 ? 'eager' : 'lazy'}
+                  className='select-none'
+                  fadeIn
                 />
-              )
-            })}
-          </nav>
-        </CarouselProvider>
-      ) : (
-        <CarouselProvider
-          naturalSlideWidth={3}
-          naturalSlideHeight={1}
-          totalSlides={desktopImages.length}
-          isPlaying
-          interval={5000}
-          infinite='true'
-        >
-          <Slider>
-            {desktopImages.map((image, index) => {
-              return (
-                <Slide index={index} key={index}>
-                  <Image
-                    fluid={image.childImageSharp.fluid}
-                    alt={`Banner ${index + 1}`}
-                    loading={index === 0 ? 'eager' : 'lazy'}
-                    className='select-none'
-                    fadeIn
-                  />
-                </Slide>
-              )
-            })}
-          </Slider>
-          <nav>
-            {desktopImages.map((image, index) => {
-              return (
-                <Dot
-                  slide={index}
-                  key={index}
-                  className='focus:outline-none focus:shadow-outline mx-1 h-3 w-3 bg-red-500 rounded-full isDisabled'
+              </Slide>
+            )
+          })}
+        </Slider>
+        <nav>
+          {desktopImages.map((image, index) => {
+            return (
+              <Dot
+                slide={index}
+                key={index}
+                className='focus:outline-none focus:shadow-outline mx-1 h-3 w-3 bg-red-500 rounded-full isDisabled'
+              />
+            )
+          })}
+        </nav>
+      </CarouselProvider>
+      <CarouselProvider
+        naturalSlideWidth={4}
+        naturalSlideHeight={3}
+        totalSlides={mobileImages.length}
+        isPlaying
+        interval={5000}
+        infinite='true'
+        className='mobileImages'
+      >
+        <Slider>
+          {mobileImages.map((image, index) => {
+            return (
+              <Slide index={index} key={index}>
+                <Image
+                  fluid={image.childImageSharp.fluid}
+                  alt={`Banner ${index + 1}`}
+                  loading={index === 0 ? 'eager' : 'lazy'}
+                  className='select-none'
+                  fadeIn
                 />
-              )
-            })}
-          </nav>
-        </CarouselProvider>
-      )}
-    </div>
+              </Slide>
+            )
+          })}
+        </Slider>
+        <nav>
+          {mobileImages.map((image, index) => {
+            return (
+              <Dot
+                slide={index}
+                key={index}
+                className='focus:outline-none focus:shadow-outline mx-1 h-3 w-3 bg-red-500 rounded-full isDisabled'
+              />
+            )
+          })}
+        </nav>
+      </CarouselProvider>
+    </>
   )
 }
 
