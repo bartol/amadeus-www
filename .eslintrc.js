a811fb140e393e813a6932b9dcddb84e5428a741
bartol <b@bartol.dev>
2020-01-24 22:47:48 +0100

    Project init with preact and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+	'gatsby-plugin-preact',
+	{
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
+  ]
+}
