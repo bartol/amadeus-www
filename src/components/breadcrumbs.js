40ebe887bdc56d39b3d0716429255d1a2ffab190
Bartol Deak <b@bdeak.net>
2020-01-25 23:40:01 +0100

    Breadcrumbs component

---
+++ src/components/breadcrumbs.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { Link } from 'gatsby';
+import { SharedContext } from '../state/shared';
+import { item } from '../locales';
+
+export const Breadcrumbs = ({ name, type, slug }) => {
+    const { language, getLanguagePrefix } = useContext(SharedContext);
+
+    return (
+        <nav>
+            <Link to={`${getLanguagePrefix(language)}/`}>
+                {item[language].breadcrumbs.home}
+            </Link>
+            {' › '}
+            <Link
+                to={`${getLanguagePrefix(language)}/${type[
+                    language
+                ].toLowerCase()}/`}
+            >
+                {type[language]}
+            </Link>
+            {' › '}
+            <Link
+                to={`${getLanguagePrefix(language)}/${type[
+                    language
+                ].toLowerCase()}/${slug}`}
+            >
+                {name[language]}
+            </Link>
+        </nav>
+    );
+};
