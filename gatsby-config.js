89f4c38eec43c424573ce89d0e2169b364f4e829
Bartol Deak <b@bdeak.net>
2020-01-25 13:34:02 +0100

    Add styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
     plugins: [
         'gatsby-plugin-preact',
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-graphql',
+            options: {
+                typeName: 'Amadeus',
+                fieldName: 'amadeus',
+                url: 'https://api.amadeus2.hr',
+            },
+        },
         {
             resolve: 'gatsby-plugin-webpack-bundle-analyzer',
             options: {
@@ -8,14 +17,6 @@ module.exports = {
                 disable: !process.env.ANALYZE_BUNDLE_SIZE,
                 generateStatsFile: true,
                 analyzerMode: 'static',
-            },
-        },
-        {
-            resolve: 'gatsby-source-graphql',
-            options: {
-                typeName: 'Amadeus',
-                fieldName: 'amadeus',
-                url: 'https://api.amadeus2.hr',
             },
         },
     ],
