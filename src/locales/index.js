f13d4f7dae81c076065909ae1950f2e973090a23
Bartol Deak <b@bdeak.net>
2020-01-26 11:37:44 +0100

    Cart translations

--- src/locales/index.js
+++ src/locales/index.js
@@ -4,3 +4,9 @@ export const item = {
     hr: hr_item,
     en: en_item,
 };
+import hr_cart from './hr/cart';
+import en_cart from './en/cart';
+export const cart = {
+    hr: hr_cart,
+    en: en_cart,
+};
