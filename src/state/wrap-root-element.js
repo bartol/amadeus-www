2c09b65f96f73707ace8b5604f52646dc416e0d2
Bartol Deak <b@bdeak.net>
2020-01-26 16:26:51 +0100

    Shared state with cart methods & others

--- src/state/wrap-root-element.js
+++ src/state/wrap-root-element.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { SharedProvider } from './shared';
 
 export const wrapRootElement = ({ element }) => {
     return <SharedProvider>{element}</SharedProvider>;
-};
-
-wrapRootElement.propTypes = {
-    element: PropTypes.element.isRequired,
 };
