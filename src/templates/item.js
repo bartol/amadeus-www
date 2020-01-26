1ee6f8e87d1437b53bdb0ea587446ae89f1ba5bc
bartol <b@bartol.dev>
2020-01-26 16:43:01 +0100

    Index, item and type pages

--- src/templates/item.js
+++ src/templates/item.js
@@ -1,7 +1,111 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { graphql } from 'gatsby';
+import { SharedContext } from '../state/shared';
+import { Layout } from '../components/layout';
+import { Breadcrumbs } from '../components/breadcrumbs';
+import { ItemImages } from '../components/itemImages';
+import { cart } from '../locales';
 
-const Item = () => {
-    return <h1>div</h1>;
+const Item = ({ data, pageContext }) => {
+    const { language } = pageContext;
+    const { item } = data.amadeus;
+
+    const { getQuantity, convertToCurrency, addToCart } = useContext(
+        SharedContext
+    );
+
+    return (
+        <Layout
+            language={language}
+            changeLanguageCustomUrl={{
+                hr: `/${item.type.hr.toLowerCase()}/${item.slug}/`,
+                en: `/en/${item.type.en.toLowerCase()}/${item.slug}/`,
+            }}
+        >
+            <Breadcrumbs name={item.name} type={item.type} slug={item.slug} />
+            <div>
+                <ItemImages images={item.images} name={item.name} />
+                <section>
+                    <h1>{item.name[language]}</h1>
+                    <h3>{getQuantity(item.id, item.quantity)}</h3>
+                    {/* availability */}
+                    <h2>{convertToCurrency(item.price)}</h2>
+                    <h2>{convertToCurrency(item.discountedPrice)}</h2>
+                    {/* guarantee... */}
+                    <button
+                        type='button'
+                        onClick={() =>
+                            addToCart({
+                                name: item.name,
+                                price: item.discountedPrice,
+                                quantity: item.quantity,
+                                availability: item.availability,
+                                image: item.images[0].optimized.fixed,
+                                id: item.id,
+                            })
+                        }
+                        disabled={getQuantity(item.id, item.quantity) === 0}
+                    >
+                        {cart[language].addToCard}
+                    </button>
+                </section>
+            </div>
+            <div>
+                <section
+                    dangerouslySetInnerHTML={{
+                        __html: item.description[language],
+                    }}
+                />
+                <aside>{/* recommended items */}</aside>
+            </div>
+        </Layout>
+    );
 };
 
 export default Item;
+
+export const itemQuery = graphql`
+    query itemQuery($id: ID!) {
+        amadeus {
+            item(id: $id) {
+                name {
+                    hr
+                    en
+                }
+                price
+                discountedPrice
+                slug
+                id
+                type {
+                    hr
+                    en
+                }
+                quantity
+                availability {
+                    hr
+                    en
+                }
+                description {
+                    hr
+                    en
+                }
+                images {
+                    index
+                    src
+                    optimized {
+                        childImageSharp {
+                            # carousel
+                            fluid(maxWidth: 800, maxHeight: 600) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                            # dots and cart
+                            fixed(width: 120, height: 90) {
+                                ...GatsbyImageSharpFixed_withWebp
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
--- src/templates/type.js
+++ src/templates/type.js
@@ -1,7 +1,88 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout } from '../components/layout';
+import { Card } from '../components/card';
 
-const Type = () => {
-    return <h1>div</h1>;
+const Type = ({ data, pageContext }) => {
+    const { language, type } = pageContext;
+
+    const items = data.amadeus.items.filter(
+        item => item.type[language] === type
+    );
+
+    return (
+        <Layout
+            language={language}
+            changeLanguageCustomUrl={{
+                hr: `/${items[0].type.hr.toLowerCase()}/`,
+                en: `/en/${items[0].type.en.toLowerCase()}/`,
+            }}
+        >
+            {/* TODO */}
+            <ul>
+                {items.map(item => {
+                    return (
+                        <Card
+                            name={item.name}
+                            price={item.price}
+                            discountedPrice={item.discountedPrice}
+                            image={item.images[0]}
+                            type={item.type}
+                            quantity={item.quantity}
+                            availability={item.availability}
+                            slug={item.slug}
+                            id={item.id}
+                            hidden={item.hidden}
+                            key={item.id}
+                        />
+                    );
+                })}
+            </ul>
+        </Layout>
+    );
 };
 
 export default Type;
+
+export const typeQuery = graphql`
+    query typeQuery {
+        amadeus {
+            items {
+                name {
+                    hr
+                    en
+                }
+                price
+                discountedPrice
+                slug
+                id
+                type {
+                    hr
+                    en
+                }
+                brand
+                quantity
+                availability {
+                    hr
+                    en
+                }
+                images {
+                    index
+                    src
+                    optimized {
+                        childImageSharp {
+                            # cards
+                            fluid(maxWidth: 240, maxHeight: 180) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                            # cart
+                            fixed(width: 120, height: 90) {
+                                ...GatsbyImageSharpFixed_withWebp
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
