a674f33a8d69b69af325055ba9c8277b89ded16f
Bartol Deak <b@bdeak.net>
2020-01-26 16:43:01 +0100

    Index, item and type pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import { Layout } from '../components/layout';
+import { Card } from '../components/card';
 
-const Index = () => {
-    return <div className='glider-contain'></div>;
+const Index = ({ pageContext, data }) => {
+    const { language } = pageContext;
+    const { items } = data.amadeus;
+
+    return (
+        <Layout language={language}>
+            {/* TODO */}
+            <ul>
+                {items.map(item => {
+                    return (
+                        <Card
+                            name={item.name}
+                            price={item.price}
+                            discountedPrice={item.discountedPrice}
+                            image={item.images[0]}
+                            type={item.type}
+                            quantity={item.quantity}
+                            availability={item.availability}
+                            slug={item.slug}
+                            id={item.id}
+                            hidden={item.hidden}
+                            key={item.id}
+                        />
+                    );
+                })}
+            </ul>
+        </Layout>
+    );
 };
 
 export default Index;
+
+export const indexQuery = graphql`
+    query indexQuery {
+        amadeus {
+            items {
+                name {
+                    hr
+                    en
+                }
+                price
+                discountedPrice
+                slug
+                id
+                type {
+                    hr
+                    en
+                }
+                brand
+                quantity
+                availability {
+                    hr
+                    en
+                }
+                images {
+                    index
+                    src
+                    optimized {
+                        childImageSharp {
+                            # cards
+                            fluid(maxWidth: 240, maxHeight: 180) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                            # cart
+                            fixed(width: 120, height: 90) {
+                                ...GatsbyImageSharpFixed_withWebp
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
