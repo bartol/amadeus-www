f7e3cda1c5ec779ef67ad13900d23e4b0ec498d7
bartol <b@bartol.dev>
2020-02-06 18:41:18 +0100

    Featured items

---
+++ src/components/featuredItems.js
@@ -0,0 +1,60 @@
+import React, { useContext, useRef, useLayoutEffect } from 'react';
+import Glider from 'glider-js';
+import { SharedContext } from '../state/shared';
+import { Card } from './card';
+
+export const FeaturedItems = ({ items }) => {
+    const { language } = useContext(SharedContext);
+
+    const gliderRef = useRef(null);
+    useLayoutEffect(() => {
+        if (!gliderRef.current) {
+            return;
+        }
+
+        new Glider(gliderRef.current, {
+            dots: '.featured-dots',
+            arrows: {
+                prev: '.featured-glider-prev',
+                next: '.featured-glider-next',
+            },
+        });
+    }, []);
+    return (
+        <div>
+            <div className='glider-wrap' ref={gliderRef}>
+                {items.map(item => {
+                    return (
+                        <Card
+                            name={item.name}
+                            price={item.price}
+                            discountedPrice={item.discountedPrice}
+                            image={item.images[0]}
+                            type={item.type}
+                            quantity={item.quantity}
+                            availability={item.availability}
+                            slug={item.slug}
+                            id={item.id}
+                            key={item.id}
+                        />
+                    );
+                })}
+            </div>
+            <button
+                role='button'
+                aria-label='Previous'
+                className='featured-glider-prev'
+            >
+                «
+            </button>
+            <button
+                role='button'
+                aria-label='Next'
+                className='featured-glider-next'
+            >
+                »
+            </button>
+            <div role='tablist' className='featured-dots'></div>
+        </div>
+    );
+};
