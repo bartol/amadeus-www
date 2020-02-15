c91ad5c24ba6a609df6b7cf99450c2d8a2dd7496
bartol <b@bartol.dev>
2020-02-15 21:51:57 +0100

    Banner navigation and styles

--- src/components/featuredItems.js
+++ src/components/featuredItems.js
@@ -50,21 +50,51 @@ export const FeaturedItems = ({ items }) => {
                     );
                 })}
             </div>
-            <button
-                role='button'
-                aria-label='Previous'
-                className='featured-glider-prev'
-            >
-                «
-            </button>
-            <button
-                role='button'
-                aria-label='Next'
-                className='featured-glider-next'
-            >
-                »
-            </button>
-            <div role='tablist' className='featured-dots'></div>
+            <nav className='carousel_nav'>
+                <button
+                    role='button'
+                    aria-label='Previous'
+                    className='featured-glider-prev'
+                >
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        stroke='currentColor'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        className='feather feather-chevrons-left'
+                    >
+                        <polyline points='11 17 6 12 11 7'></polyline>
+                        <polyline points='18 17 13 12 18 7'></polyline>
+                    </svg>
+                </button>
+                <div role='tablist' className='featured-dots'></div>
+                <button
+                    role='button'
+                    aria-label='Next'
+                    className='featured-glider-next'
+                >
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='24'
+                        height='24'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        stroke='currentColor'
+                        strokeWidth='2'
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        className='feather feather-chevrons-right'
+                    >
+                        <polyline points='13 17 18 12 13 7'></polyline>
+                        <polyline points='6 17 11 12 6 7'></polyline>
+                    </svg>
+                </button>
+            </nav>
         </div>
     );
 };
