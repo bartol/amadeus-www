ef5c91ad925cae651554b5fd2937b60e39d9e036
Bartol Deak <b@bdeak.net>
2020-02-16 00:14:01 +0100

    Fix scroll jumping on state change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,3 +6,15 @@ import './src/styles/styles.scss';
 
 // glider-js styles
 import 'glider-js/glider.min.css';
+
+const shouldUpdateScroll = ({
+    routerProps: { location },
+    getSavedScrollPosition,
+}) => {
+    const currentPosition = getSavedScrollPosition(location);
+
+    window.scrollTo(...(currentPosition || [0, 0]));
+
+    return false;
+};
+export { shouldUpdateScroll };
