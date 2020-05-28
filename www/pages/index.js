17e983e14c52170f3003a2acdd9678426d873fb3
Bartol Deak <b@bdeak.net>
2020-05-28 22:52:14 +0200

    Tree nav work in progress

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import Head from "next/head";
 import useSWR from "swr";
 import Link from "next/link";
@@ -12,16 +13,54 @@ import {
   AccordionHeader,
   AccordionPanel,
   AccordionIcon,
+  Drawer,
+  useDisclosure,
+  Button,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  Input,
 } from "@chakra-ui/core";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
+
+function createNode(path, tree) {
+  const name = path.shift();
+  const idx = tree.findIndex((e) => {
+    return e.name == name;
+  });
+  if (idx < 0) {
+    tree.push({
+      name: name,
+      children: [],
+    });
+    if (path.length !== 0) {
+      createNode(path, tree[tree.length - 1].children);
+    }
+  } else {
+    createNode(path, tree[idx].children);
+  }
+}
+
+function getTree(categories) {
+  const tree = [];
+  for (let i = 0; i < categories.length; i++) {
+    const path = categories[i];
+    const split = path.Categories.map((c) => c.Slug);
+    createNode(split, tree);
+  }
+  return tree;
+}
 
 function TreeNav({ categories }) {
   return (
     <Accordion allowMultiple>
       {categories &&
         categories
-          .sort((a, b) => a.Products.length < b.Products.length)
+          // .sort((a, b) => a.Products.length < b.Products.length)
           .map((c) => {
             return <TreeNavItem category={c} />;
           })}
@@ -30,25 +69,33 @@ function TreeNav({ categories }) {
 }
 
 function TreeNavItem({ category }) {
-  return (
+  return category.children.length ? (
     <AccordionItem>
       <AccordionHeader>
         <Box flex="1" textAlign="left">
-          {category.Name}
+          {category.name}
         </Box>
         <AccordionIcon />
       </AccordionHeader>
       <AccordionPanel pb={4}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        {category.children.map((cc) => (
+          <TreeNavItem category={cc} />
+        ))}
       </AccordionPanel>
     </AccordionItem>
+  ) : (
+    <Box px={4} py={2}>
+      <Link href={`/${category.name}`}>
+        <ChakraLink href={category.name}>{category.name}</ChakraLink>
+      </Link>
+    </Box>
   );
 }
 
 function Index() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
   const { data: categories, error } = useSWR(
     "http://localhost:8080/categories/",
     fetcher
@@ -71,6 +118,8 @@ function Index() {
     });
   });
 
+  var tree = getTree(categories);
+
   return (
     <div className="container">
       <Head>
@@ -80,11 +129,37 @@ function Index() {
       <Icon name="search" color="red.500" />
       <h1>amadeus2.hr</h1>
 
-      <pre>DEBUG: {JSON.stringify(ctgs, null, 2)}</pre>
+      <Button ref={btnRef} variantColor="teal" onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Proizvodi</DrawerHeader>
+
+          <DrawerBody>
+            <TreeNav categories={tree[0].children} />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button color="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
 
       <h2>---</h2>
 
-      <TreeNav categories={categories} />
+      <pre>DEBUG: {JSON.stringify(tree, null, 2)}</pre>
+      {/* <pre>DEBUG: {JSON.stringify(categories, null, 2)}</pre> */}
 
       <h2>---</h2>
 
