3b76f9dc19528095b47e0e8907540027521a50d8
Bartol Deak <b@bdeak.net>
2020-06-01 16:10:44 +0200

    remove chakra-ui, add tailwindcss

--- www/pages/_app.js
+++ www/pages/_app.js
@@ -1,16 +1,7 @@
-import NextApp from "next/app";
-import { CSSReset, ThemeProvider } from "@chakra-ui/core";
+import "./styles.css";
 
-class App extends NextApp {
-  render() {
-    const { Component } = this.props;
-    return (
-      <ThemeProvider>
-        <CSSReset />
-        <Component />
-      </ThemeProvider>
-    );
-  }
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default App;
