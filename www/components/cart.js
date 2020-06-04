07b6a1e51da273af18307fa0eb3c573879f5196c
bartol <b@bartol.dev>
2020-06-04 17:32:11 +0200

    cart table improvements

--- www/components/cart.js
+++ www/components/cart.js
@@ -7,60 +7,69 @@ function Cart({ cart, setCart }) {
   return (
     <Drawer placement="right">
       <table className="table">
-        {cart.map((p) => {
-          return (
-            <tr key={p.ID}>
-              <td>
-                <Link href={"/" + p.URL}>
-                  <a>
-                    <img src={p.DefaultImage.URL} alt="slika proizvoda" className="w-20" />
-                  </a>
-                </Link>
-              </td>
-              <td>
-                <Link href={"/" + p.URL}>
-                  <a>{p.Name}</a>
-                </Link>
-              </td>
-              <td>
-                <div>
+        <thead>
+          <tr>
+            <th colSpan="2">Naziv</th>
+            <th>Cijena</th>
+            <th colSpan="2">Količina</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((p) => {
+            return (
+              <tr key={p.ID}>
+                <td>
+                  <Link href={"/" + p.URL}>
+                    <a>
+                      <img src={p.DefaultImage.URL} alt="slika proizvoda" className="w-20" />
+                    </a>
+                  </Link>
+                </td>
+                <td>
+                  <Link href={"/" + p.URL}>
+                    <a>{p.Name}</a>
+                  </Link>
+                </td>
+                <td>
                   <div>
-                    <h4 className={`${p.HasReduction ? "line-through" : "text-2xl"}`}>
+                    <h4 className={`${p.HasReduction ? "line-through" : "subheading"}`}>
                       {getPrice(p.Price)}
                     </h4>
-                    {p.HasReduction && (
-                      <h4 className="text-2xl">
-                        {getReductionPrice(p.Price, p.Reduction, p.ReductionType)}
-                      </h4>
-                    )}
-                  </div>
+                    <div className="flex">
+                      {p.HasReduction && (
+                        <h4 className="subheading">
+                          {getReductionPrice(p.Price, p.Reduction, p.ReductionType)}
+                        </h4>
+                      )}
 
-                  {p.HasReduction && (
-                    <h4 className="text-2xl">{getReduction(p.Reduction, p.ReductionType)}</h4>
-                  )}
-                </div>
-              </td>
-              <td>
-                <input
-                  type="number"
-                  value={p.Quantity}
-                  onChange={(e) => CartSetQuantity(setCart, p, e.target.value | 0)}
-                  min="1"
-                  className="input"
-                />
-              </td>
-              <td>
-                <button
-                  type="button"
-                  onClick={() => CartRemove(setCart, p)}
-                  className="button ~critical !normal"
-                >
-                  izbaci iz kosarice{/* TODO icon */}
-                </button>
-              </td>
-            </tr>
-          );
-        })}
+                      {p.HasReduction && (
+                        <h4 className="subheading">{getReduction(p.Reduction, p.ReductionType)}</h4>
+                      )}
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <input
+                    type="number"
+                    value={p.Quantity}
+                    onChange={(e) => CartSetQuantity(setCart, p, e.target.value | 0)}
+                    min="1"
+                    className="input"
+                  />
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    onClick={() => CartRemove(setCart, p)}
+                    className="button ~critical !normal"
+                  >
+                    izbaci iz kosarice{/* TODO icon */}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
       <hr />
       <h4 className="text-2xl">Total: {getTotal(cart)}</h4>
