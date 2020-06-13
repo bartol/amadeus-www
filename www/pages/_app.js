c8b6708b6a2857b0fc8d6ae911a40d3b44580082
Bartol Deak <b@bdeak.net>
2020-06-13 19:55:51 +0200

    add images

--- www/pages/_app.js
+++ www/pages/_app.js
@@ -4,14 +4,15 @@ import { cartGet } from "../helpers/cart";
 import "rc-drawer/assets/index.css";
 import "rc-slider/assets/index.css";
 import "a17t";
-import "../public/styles.css";
+import "../public/css/styles.css";
+import Header from "../components/header";
 
 function App({ Component, pageProps }) {
   const [cart, setCart] = useState(cartGet());
 
   return (
     <div className="bg-gray-100">
-      Amadeus II shop
+      <Header />
       <Component {...pageProps} cart={cart} setCart={setCart} />
       <Cart cart={cart} setCart={setCart} />
     </div>
