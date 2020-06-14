a3a06df48ee7fced21c3751e4bfbb94761fbe0a1
Bartol Deak <b@bdeak.net>
2020-06-14 08:55:41 +0200

    remove showCategory prop

--- www/components/product_list.js
+++ www/components/product_list.js
@@ -4,8 +4,8 @@ import { useState, useEffect } from "react";
 import { Range } from "rc-slider";
 import { getPrice } from "../helpers/price";
 
-function ProductList({ products, pagination, pageSize, showCategories, setCart }) {
-  if (!products.length) {
+function ProductList({ products, pagination, pageSize, setCart }) {
+  if (!products || !products.length) {
     return <div>no products</div>;
   }
 
@@ -25,12 +25,12 @@ function ProductList({ products, pagination, pageSize, showCategories, setCart }
   const [filteredList, setFilteredList] = useState(products);
   useEffect(() => {
     setFilteredList(getFilteredList(products, selected, filters.price));
-  }, [selected]);
+  }, [products, selected]);
 
   return (
     <div className="flex lg:flex-row flex-col">
       <div className="xl:w-1/6 lg:w-1/4 mr-5">
-        {showCategories && (
+        {filters.categories.length > 1 && (
           <div>
             <h3 className="subheading">Kategorije</h3>
             <div>
@@ -57,62 +57,66 @@ function ProductList({ products, pagination, pageSize, showCategories, setCart }
             </div>
           </div>
         )}
-        <h3 className="subheading">Značajke</h3>
-        <div>
-          {filters.features.map((f) => {
-            return (
-              <div className="inline" key={f.name}>
-                <button
-                  type="button"
-                  onClick={() => {
-                    const name = selected.feature.name !== f.name ? f.name : "";
-                    setSelected({
-                      ...selected,
-                      feature: {
-                        name,
-                        value: "",
-                      },
-                    });
-                  }}
-                  className={`button ${
-                    selected.feature.name === f.name ? "~positive" : "~neutral"
-                  } !normal m-1`}
-                >
-                  {f.name}
-                </button>
-                {selected.feature.name === f.name && (
-                  <div>
-                    <hr className="m-1" />
-                    {f.values.map((v) => {
-                      return (
-                        <button
-                          type="button"
-                          onClick={() => {
-                            const value = selected.feature.value !== v ? v : "";
-                            setSelected({
-                              ...selected,
-                              feature: {
-                                ...selected.feature,
-                                value,
-                              },
-                            });
-                          }}
-                          className={`button ${
-                            selected.feature.value === v ? "~urge" : "~neutral"
-                          } !normal m-1`}
-                          key={v}
-                        >
-                          {v}
-                        </button>
-                      );
-                    })}
-                    <hr className="m-1" />
+        {filters.features.length > 0 && (
+          <div>
+            <h3 className="subheading">Značajke</h3>
+            <div>
+              {filters.features.map((f) => {
+                return (
+                  <div className="inline" key={f.name}>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        const name = selected.feature.name !== f.name ? f.name : "";
+                        setSelected({
+                          ...selected,
+                          feature: {
+                            name,
+                            value: "",
+                          },
+                        });
+                      }}
+                      className={`button ${
+                        selected.feature.name === f.name ? "~positive" : "~neutral"
+                      } !normal m-1`}
+                    >
+                      {f.name}
+                    </button>
+                    {selected.feature.name === f.name && (
+                      <div>
+                        <hr className="m-1" />
+                        {f.values.map((v) => {
+                          return (
+                            <button
+                              type="button"
+                              onClick={() => {
+                                const value = selected.feature.value !== v ? v : "";
+                                setSelected({
+                                  ...selected,
+                                  feature: {
+                                    ...selected.feature,
+                                    value,
+                                  },
+                                });
+                              }}
+                              className={`button ${
+                                selected.feature.value === v ? "~urge" : "~neutral"
+                              } !normal m-1`}
+                              key={v}
+                            >
+                              {v}
+                            </button>
+                          );
+                        })}
+                        <hr className="m-1" />
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
-            );
-          })}
-        </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
         <h3 className="subheading">Cijena</h3>
         <div className="mx-3 my-1">
           <Range
