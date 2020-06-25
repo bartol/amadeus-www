92298f20905641d62c8c5b053db5e1ba95d22341
bartol <b@bartol.dev>
2020-06-25 21:31:06 +0200

    add default path

--- www/components/seo.js
+++ www/components/seo.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-function SEO({ title, description, image, path }) {
+function SEO({ title, description, image, path = "" }) {
   return (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
