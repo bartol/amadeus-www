386cbea9967a937dcb82dd7f09b45f4ceaa137e4
bartol <b@bartol.dev>
2020-06-27 09:34:13 +0200

    /search detect when query isn't present

--- www/pages/search.js
+++ www/pages/search.js
@@ -5,6 +5,7 @@ import SEO from "../components/seo.js";
 function Search({
   query,
   results,
+  hasResults,
   categoriesTree,
   setCart,
   menuOpened,
@@ -13,31 +14,47 @@ function Search({
 }) {
   return (
     <div className="container mx-auto px-4">
-      <SEO
-        title={`Rezultati pretrage "${query}" | Amadeus II d.o.o. shop`}
-        description={`Rezultati pretrage "${query}"`}
-        path={`search/?q=${encodeURIComponent(query)}`}
-      />
-      <h1 className="heading text-4xl mt-12 mb-5">Rezultati pretrage "{query}"</h1>
-      <ProductList products={results} setCart={setCart} dispatchAlert={dispatchAlert} />
+      {hasResults ? (
+        <div>
+          <SEO
+            title={`Rezultati pretrage "${query}" | Amadeus II d.o.o. shop`}
+            description={`Rezultati pretrage "${query}"`}
+            path={`search/?q=${encodeURIComponent(query)}`}
+          />
+          <h1 className="heading text-4xl mt-12 mb-5">Rezultati pretrage "{query}"</h1>
+          <ProductList products={results} setCart={setCart} dispatchAlert={dispatchAlert} />
+        </div>
+      ) : (
+        <div>search box</div>
+      )}
       <Menu categories={categoriesTree} menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
     </div>
   );
 }
 
-export async function getServerSideProps({ query }) {
-  const resultsData = await fetch(
-    `https://api.amadeus2.hr/search/?query=${encodeURIComponent(query.q)}`
-  );
-  const results = await resultsData.json();
-
+export async function getServerSideProps({ query: { q } }) {
   const categoriesTreeRes = await fetch("https://api.amadeus2.hr/categories/tree");
   const categoriesTree = await categoriesTreeRes.json();
 
+  if (!q) {
+    return {
+      props: {
+        query: "",
+        results: [],
+        hasResults: false,
+        categoriesTree,
+      },
+    };
+  }
+
+  const resultsData = await fetch(`https://api.amadeus2.hr/search/?query=${encodeURIComponent(q)}`);
+  const results = await resultsData.json();
+
   return {
     props: {
-      query: query.q,
+      query: q,
       results,
+      hasResults: true,
       categoriesTree,
     },
   };
