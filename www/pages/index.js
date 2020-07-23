50d15d031145e806568e9cb7d8b02f4726b2e198
Bartol Deak <b@bartol.dev>
2020-07-23 17:43:27 +0200

    category cards

--- www/pages/index.js
+++ www/pages/index.js
@@ -3,6 +3,7 @@ import ProductList from "../components/product_list.js";
 import Menu from "../components/menu";
 import Info from "../components/info.js";
 import SEO from "../components/seo.js";
+import { ExternalLink } from "react-feather";
 
 function Index({
   products,
@@ -28,15 +29,40 @@ function Index({
             <li key={c.ID}>
               <Link href="/[category]" as={"/" + c.Slug}>
                 <a>
-                  <div className="card ~neutral !low h-56">
+                  <div className="card ~neutral !low relative h-48">
                     <h3 className="subheading text-2xl">{c.Name}</h3>
-                    <h4 className="subheading">{c.Products.length} proizvoda</h4>
+                    <h4>{c.Products.length} proizvoda</h4>
+                    {c.Image.URL && (
+                      <img
+                        src={c.Image.URL}
+                        alt="slika kategorije"
+                        className="absolute right-0 bottom-0 m-4"
+                        width="120px"
+                        height="120px"
+                      />
+                    )}
                   </div>
                 </a>
               </Link>
             </li>
           );
         })}
+        <li>
+          <a href="https://pioneer.hr" target="_blank">
+            <div className="card ~neutral !low relative h-48">
+              <div className="flex justify-between items-baseline">
+                <h3 className="subheading text-2xl truncate">PioneerDJ Hrvatska</h3>
+                <ExternalLink />
+              </div>
+              <h4>Autorizirani shop PioneerDJ opreme i uređaja za Hrvatsku.</h4>
+              <img
+                src="https://pioneer.hr/img/my-shop-logo-1430300787.jpg"
+                alt="pioneer.hr logo"
+                className="absolute right-0 bottom-0 m-4 mb-5"
+              />
+            </div>
+          </a>
+        </li>
       </ul>
       <h2 className="heading text-4xl mt-12 mb-5">Izdvojeni proizvodi</h2>
       <ProductList
@@ -57,8 +83,9 @@ export async function getStaticProps() {
   const products = categories.find((c) => c.Slug === "amadeus-ii-shop").Products;
   categories = categories
     .filter((c) => c.Slug !== "amadeus-ii-shop")
+    .filter((c) => c.Image.URL !== "")
     .sort((a, b) => b.Products.length - a.Products.length)
-    .slice(0, 6);
+    .slice(0, 5);
 
   const categoriesTreeRes = await fetch("https://api.amadeus2.hr/categories/tree");
   const categoriesTree = await categoriesTreeRes.json();
