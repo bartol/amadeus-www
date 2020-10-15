043745a23f4318d84325f24e81aceef6ff631a81
Bartol Deak <b@bdeak.net>
2020-10-15 12:38:59 +0200

    stop process before transfering exe to srv

--- api/update.sh
+++ api/update.sh
@@ -2,10 +2,16 @@
 
 git push
 go build
+
+ssh srv1 << EOF
+sudo systemctl stop amadeus2.hr-api.service
+cd amadeus-www
+git reset --hard HEAD
+git pull
+EOF
+
 scp api srv1:/home/ubuntu/amadeus-www/api
 
 ssh srv1 << EOF
-cd amadeus-www
-git pull
-sudo systemctl restart amadeus2.hr-api.service
+sudo systemctl start amadeus2.hr-api.service
 EOF
