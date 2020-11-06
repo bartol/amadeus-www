a89cdd71b64fd50a035f35688552e5c6d3b321ce
Bartol Deak <b@bdeak.net>
2020-11-06 19:26:34 +0100

    remove python prototype

--- kasa/src/gui.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import webview
-from server import server
-
-if __name__ == '__main__':
-  window = webview.create_window('amadeus-kasa', server, min_size=(1200, 800))
-  webview.start(debug=True)
--- kasa/src/server.py
+++
@@ -1,8 +0,0 @@
-from core.message import *
-from flask import Flask
-
-server = Flask(__name__)
-
-@server.route('/')
-def landing():
-    return getmessage()
