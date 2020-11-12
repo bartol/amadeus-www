42016ec4f8e47cb8a7119b7cf94001f1481b877c
Bartol Deak <b@bdeak.net>
2020-11-12 17:22:04 +0100

    add product function stubs

--- kasa/core/products.go
+++ kasa/core/products.go
@@ -1,5 +1,21 @@
 package core
 
-func ProductFunc() string {
-	return "product func"
+func ProductGet(product_id string) string {
+	return "stub"
+}
+
+func ProductGetAll() string {
+	return "stub"
+}
+
+func ProductGetModified(products string) string {
+	return "stub"
+}
+
+func ProductCreate(product string) string {
+	return "stub"
+}
+
+func ProductUpdate(product string) string {
+	return "stub"
 }
