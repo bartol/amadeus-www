45230ee1186b14f83f33191709c8c60fbf2a3344
Bartol Deak <b@bdeak.net>
2020-11-16 19:02:21 +0100

    add tests for ProductGetListSlim and its golden files

--- kasa/core/products_test.go
+++ kasa/core/products_test.go
@@ -39,3 +39,33 @@ func TestProductGet(t *testing.T) {
 		}
 	}
 }
+
+func TestProductGetListSlim(t *testing.T) {
+	var cases = []struct {
+		offset int
+		limit  int
+	}{
+		{0, 2},
+		{2, 6},
+		{50, 2},
+	}
+
+	for _, tc := range cases {
+		actual := ProductGetListSlim(tc.offset, tc.limit)
+		goldenout := "./testdata/products/ProductGetListSlim/list_" + strconv.Itoa(tc.offset) + "_" + strconv.Itoa(tc.limit) + ".out.golden"
+		goldenoutfail := "./testdata/products/ProductGetListSlim/list_" + strconv.Itoa(tc.offset) + "_" + strconv.Itoa(tc.limit) + ".outfail.golden"
+		os.Remove(goldenoutfail)
+		if *Update {
+			ioutil.WriteFile(goldenout, []byte(actual), 0644)
+		}
+
+		expected, err := ioutil.ReadFile(goldenout)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal([]byte(actual), expected) {
+			ioutil.WriteFile(goldenoutfail, []byte(actual), 0644)
+			t.Errorf("actual (%s) didn't match golden (%s) ", goldenoutfail, goldenout)
+		}
+	}
+}
