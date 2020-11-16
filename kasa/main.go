fdf4ae13d59a5c90c539cd670777fcb240411f41
Bartol Deak <b@bdeak.net>
2020-11-16 18:07:39 +0100

    cleanup main files

--- kasa/main.go
+++ kasa/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "amadeus-kasa/core"
-  "log"
+	"amadeus-kasa/core"
+	"log"
 
-  "github.com/leaanthony/mewn"
-  "github.com/wailsapp/wails"
+	"github.com/leaanthony/mewn"
+	"github.com/wailsapp/wails"
 )
 
 func basic() string {
-  return "Hello World!"
+	return "Hello World!"
 }
 
 func main() {
-  err := core.SetupEnv()
-  if err != nil {
-    log.Fatalln(err)
-  }
+	err := core.Setup("postgres://postgres@localhost:5432/kasa14")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-  js := mewn.String("./frontend/dist/app.js")
-  css := mewn.String("./frontend/dist/app.css")
+	js := mewn.String("./frontend/dist/app.js")
+	css := mewn.String("./frontend/dist/app.css")
 
-  app := wails.CreateApp(&wails.AppConfig{
-    Width:     1400,
-    Height:    900,
-    Title:     "amadeus-kasa",
-    JS:        js,
-    CSS:       css,
-    Colour:    "#131313",
-    Resizable: true,
-  })
-  app.Bind(basic)
-  app.Bind(core.ProductGet)
-  app.Run()
+	app := wails.CreateApp(&wails.AppConfig{
+		Width:     1400,
+		Height:    900,
+		Title:     "amadeus-kasa",
+		JS:        js,
+		CSS:       css,
+		Colour:    "#131313",
+		Resizable: true,
+	})
+	app.Bind(basic)
+	app.Bind(core.ProductGet)
+	app.Run()
 }
