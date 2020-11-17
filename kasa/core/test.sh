9751a7c0333fc8980f28b4ec6cb14a22ab4fd4ca
Bartol Deak <b@bdeak.net>
2020-11-17 16:30:38 +0100

    remove time sensitive data from golden files

--- kasa/core/test.sh
+++ kasa/core/test.sh
@@ -11,8 +11,6 @@ trap cleanup EXIT
 
 psql -U postgres -c "CREATE DATABASE $DB_NAME"
 
-echo "DB_NAME=$DB_NAME"
-
 psql_run() {
 	psql -U postgres -d $DB_NAME -c "$1"
 }
@@ -21,3 +19,13 @@ psql_run "\i ./db/schema.sql"
 psql_run "\i ./db/seed.sql"
 
 go test -v -dbconn "postgres://postgres@localhost:5432/$DB_NAME" $@
+
+sed_run() {
+	find $1 -type f -name "*.out.golden" -print0 | 
+	xargs -0 sed -i "$2"
+}
+
+# remove time sensitive data from golden files
+sed_run ./testdata/products/ProductCreate "/created_at/d"
+sed_run ./testdata/products/ProductCreate "/updated_at/d"
+sed_run ./testdata/products/ProductUpdate "/updated_at/d"
