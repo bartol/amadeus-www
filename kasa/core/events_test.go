e40a7d3a36479546733524eac56e3b309c2589e2
Bartol Deak <b@bdeak.net>
2020-11-17 20:40:34 +0100

    add tests for EventGetList

--- kasa/core/events_test.go
+++ kasa/core/events_test.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"strconv"
-	"strings"
 	"testing"
 )
 
@@ -23,19 +21,40 @@ func TestEventGet(t *testing.T) {
 	for _, tc := range cases {
 		t.Run("event_"+strconv.Itoa(tc.eventID), func(t *testing.T) {
 			actual := EventGet(tc.eventID)
+			goldenout := "./testdata/events/EventGet/event_" + strconv.Itoa(tc.eventID) + ".out.golden"
+			goldenoutfail := "./testdata/events/EventGet/event_" + strconv.Itoa(tc.eventID) + ".outfail.golden"
+			os.Remove(goldenoutfail)
+			if *Update {
+				ioutil.WriteFile(goldenout, []byte(actual), 0644)
+			}
 
-			// remove dynamic fields
-			cmd := "sed /created_at/d"
-			cmdexec := exec.Command("bash", "-c", cmd)
-			cmdexec.Stdin = strings.NewReader(actual)
-			out, err := cmdexec.Output()
+			expected, err := ioutil.ReadFile(goldenout)
 			if err != nil {
 				t.Fatal(err)
 			}
-			actual = string(out)
+			if !bytes.Equal([]byte(actual), expected) {
+				ioutil.WriteFile(goldenoutfail, []byte(actual), 0644)
+				t.Errorf("actual (%s) didn't match golden (%s) ", goldenoutfail, goldenout)
+			}
+		})
+	}
+}
 
-			goldenout := "./testdata/events/EventGet/event_" + strconv.Itoa(tc.eventID) + ".out.golden"
-			goldenoutfail := "./testdata/events/EventGet/event_" + strconv.Itoa(tc.eventID) + ".outfail.golden"
+func TestEventGetList(t *testing.T) {
+	var cases = []struct {
+		offset int
+		limit  int
+	}{
+		{0, 2},
+		{2, 6},
+		{50, 2},
+	}
+
+	for _, tc := range cases {
+		t.Run("list_"+strconv.Itoa(tc.offset)+"_"+strconv.Itoa(tc.limit), func(t *testing.T) {
+			actual := EventGetList(tc.offset, tc.limit)
+			goldenout := "./testdata/events/EventGetList/list_" + strconv.Itoa(tc.offset) + "_" + strconv.Itoa(tc.limit) + ".out.golden"
+			goldenoutfail := "./testdata/events/EventGetList/list_" + strconv.Itoa(tc.offset) + "_" + strconv.Itoa(tc.limit) + ".outfail.golden"
 			os.Remove(goldenoutfail)
 			if *Update {
 				ioutil.WriteFile(goldenout, []byte(actual), 0644)
