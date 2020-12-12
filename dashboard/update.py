093bb57c4b9b973a8e2d2651be654d8bee1229ec
Bartol Deak <b@bdeak.net>
2020-12-12 14:47:39 +0100

    extract parsing products in its function

--- dashboard/update.py
+++ dashboard/update.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import csv, os, shutil
+import csv
+import os, shutil
 
 bazadir = 'baza'
 bazatmp = 'baza-tmp'
@@ -14,27 +15,40 @@ shutil.copyfile(f"{bazadir}/POD1/{year}/malst.tps", f"{bazatmp}/malst.tps")
 os.system(f"java -jar tps-to-csv.jar -s {bazatmp}/malmat.tps -t {bazatmp}/malmat.csv")
 os.system(f"java -jar tps-to-csv.jar -s {bazatmp}/malst.tps -t {bazatmp}/malst.csv")
 
-with open('baza-tmp/malmat.csv', encoding='cp852') as f:
-    reader = csv.reader(f)
-    next(reader)
-    sviproizvodi = list(reader)
 
-with open('baza-tmp/malst.csv', encoding='cp852') as f:
-    reader = csv.reader(f)
-    next(reader)
-    aktivniproizvodi = list(reader)
+def parseproducts(malmatpath, malstpath):
 
-for p in aktivniproizvodi:
-    try:
-        p2 = next(p2 for p2 in sviproizvodi if p2[1] == p[2])
-    except:
-        continue
+    with open(malmatpath, encoding='cp852') as f:
+        reader = csv.reader(f)
+        next(reader)
+        sviproizvodi = list(reader)
 
-    print("sifra:", p[2])
-    print("grupa:", p2[3])
-    print("naziv:", p2[4].rstrip())
-    print("kolicina:", p[14])
-    print("nabavna_cijena:", p[18])
-    print("marza%:", p[16])
-    print("cijena:", p[9])
-    print("rabat%:", p[15])
+    with open(malstpath, encoding='cp852') as f:
+        reader = csv.reader(f)
+        next(reader)
+        aktivniproizvodi = list(reader)
+
+    products = []
+
+    for p in aktivniproizvodi:
+        try:
+            p2 = next(p2 for p2 in sviproizvodi if p2[1] == p[2])
+        except:
+            continue
+
+        products.append({
+            "sifra": int(p[2]),
+            "grupa": int(p2[3]),
+            "naziv": p2[4].rstrip(),
+            "kolicina": int(float(p[14])),
+            "nabavna_cijena": float(p[18]),
+            "marza": float(p[16]),
+            "cijena": float(p[9]),
+            "rabat": float(p[15])
+        })
+
+    return products
+
+
+products = parseproducts(f'{bazatmp}/malmat.csv', f'{bazatmp}/malst.csv')
+print(products)
