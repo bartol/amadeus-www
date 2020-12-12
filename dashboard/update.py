84afa6b3b8e9f57d343e3277bbcd510779f1d516
Bartol Deak <b@bdeak.net>
2020-12-12 16:36:11 +0100

    finish simplified version

--- dashboard/update.py
+++ dashboard/update.py
@@ -9,39 +9,45 @@ bazatmpdir = 'baza-tmp'
 bazacachedir = 'baza-cache'
 year = 2017
 
-if not os.path.exists(bazatmpdir): os.mkdir(bazatmpdir)
-if not os.path.exists(bazacachedir): os.mkdir(bazacachedir)
+if not os.path.exists(bazatmpdir):
+    os.mkdir(bazatmpdir)
+if not os.path.exists(bazacachedir):
+    os.mkdir(bazacachedir)
 
 shutil.copyfile(f"{bazadir}/POD1/MALMAT.TPS", f"{bazatmpdir}/malmat.tps")
 shutil.copyfile(f"{bazadir}/POD1/{year}/malst.tps", f"{bazatmpdir}/malst.tps")
+
 os.system(f"java -jar tps-to-csv.jar -s {bazatmpdir}/malmat.tps -t {bazatmpdir}/malmat.csv")
 os.system(f"java -jar tps-to-csv.jar -s {bazatmpdir}/malst.tps -t {bazatmpdir}/malst.csv")
-
-with open(f'{bazatmpdir}/malmat.csv', encoding='cp852') as f:
-    reader = csv.reader(f)
-    next(reader)
-    proizvodi = list(reader)
 
 with open(f'{bazatmpdir}/malst.csv', encoding='cp852') as f:
     reader = csv.reader(f)
     next(reader)
-    for p1 in reader:
-        try:
-            p2 = next(item for item in proizvodi if item[1] == p1[2])
-        except:
+    cijene = list(reader)
+
+with open(f'{bazatmpdir}/malmat.csv', encoding='cp852') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for p in reader:
+        c = []
+        for cj in cijene:
+            if cj[2] == p[1]:
+                c = cj
+        if len(c) == 0:
             continue
 
         product = {
-            'sifra': int(p1[2]),
-            'grupa': int(p2[3]),
-            'naziv': p2[4].rstrip(),
-            'kolicina': int(float(p1[14])),
-            'nabavna_cijena': float(p1[18]),
-            'marza': float(p1[16]),
-            'cijena': float(p1[9]),
-            'rabat': float(p1[15])
+            'sifra': int(p[1]),
+            'grupa': int(p[3]),
+            'naziv': p[4].rstrip(),
+            'kolicina': int(float(c[14])),
+            'nabavna_cijena': float(c[18]),
+            'marza': float(c[16]),
+            'cijena': float(c[9]),
+            'rabat': float(c[15])
         }
 
+        new = True
         cachepath = f"{bazacachedir}/{product['sifra']}"
         if os.path.exists(cachepath):
             with open(cachepath) as cf:
@@ -49,8 +55,16 @@ with open(f'{bazatmpdir}/malst.csv', encoding='cp852') as f:
                 if cachedproduct == product:
                     continue
                 else:
-                    print(product['sifra'])
-                    print(set(cachedproduct.items()) ^ set(product.items()))
+                    diff = set(cachedproduct.items()) ^ set(product.items())
+                    print('IZMJENJEN PROIZVOD', product['sifra'], diff)
+                    new = False
+
+        if new:
+            # insert product
+            print('NOVI PROIZVOD', product['sifra'], product['naziv'])
+        else:
+            # update product
+            pass
 
         with open(cachepath, 'w') as cf:
             json.dump(product, cf)
