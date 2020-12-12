70922f1d796f743d076a41f4b3cfbb45669f146d
Bartol Deak <b@bdeak.net>
2020-12-12 16:16:37 +0100

    simplifiing (wip)

--- dashboard/update.py
+++ dashboard/update.py
@@ -1,54 +1,56 @@
 #!/usr/bin/env python3
 
 import csv
+import json
 import os, shutil
 
 bazadir = 'baza'
-bazatmp = 'baza-tmp'
-
+bazatmpdir = 'baza-tmp'
+bazacachedir = 'baza-cache'
 year = 2017
 
-if not os.path.exists(bazatmp):
-    os.mkdir(bazatmp)
-shutil.copyfile(f"{bazadir}/POD1/MALMAT.TPS", f"{bazatmp}/malmat.tps")
-shutil.copyfile(f"{bazadir}/POD1/{year}/malst.tps", f"{bazatmp}/malst.tps")
-os.system(f"java -jar tps-to-csv.jar -s {bazatmp}/malmat.tps -t {bazatmp}/malmat.csv")
-os.system(f"java -jar tps-to-csv.jar -s {bazatmp}/malst.tps -t {bazatmp}/malst.csv")
+if not os.path.exists(bazatmpdir): os.mkdir(bazatmpdir)
+if not os.path.exists(bazacachedir): os.mkdir(bazacachedir)
 
+shutil.copyfile(f"{bazadir}/POD1/MALMAT.TPS", f"{bazatmpdir}/malmat.tps")
+shutil.copyfile(f"{bazadir}/POD1/{year}/malst.tps", f"{bazatmpdir}/malst.tps")
+os.system(f"java -jar tps-to-csv.jar -s {bazatmpdir}/malmat.tps -t {bazatmpdir}/malmat.csv")
+os.system(f"java -jar tps-to-csv.jar -s {bazatmpdir}/malst.tps -t {bazatmpdir}/malst.csv")
 
-def parseproducts(malmatpath, malstpath):
+with open(f'{bazatmpdir}/malmat.csv', encoding='cp852') as f:
+    reader = csv.reader(f)
+    next(reader)
+    proizvodi = list(reader)
 
-    with open(malmatpath, encoding='cp852') as f:
-        reader = csv.reader(f)
-        next(reader)
-        sviproizvodi = list(reader)
-
-    with open(malstpath, encoding='cp852') as f:
-        reader = csv.reader(f)
-        next(reader)
-        aktivniproizvodi = list(reader)
-
-    products = []
-
-    for p in aktivniproizvodi:
+with open(f'{bazatmpdir}/malst.csv', encoding='cp852') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for p1 in reader:
         try:
-            p2 = next(p2 for p2 in sviproizvodi if p2[1] == p[2])
+            p2 = next(item for item in proizvodi if item[1] == p1[2])
         except:
             continue
 
-        products.append({
-            "sifra": int(p[2]),
-            "grupa": int(p2[3]),
-            "naziv": p2[4].rstrip(),
-            "kolicina": int(float(p[14])),
-            "nabavna_cijena": float(p[18]),
-            "marza": float(p[16]),
-            "cijena": float(p[9]),
-            "rabat": float(p[15])
-        })
+        product = {
+            'sifra': int(p1[2]),
+            'grupa': int(p2[3]),
+            'naziv': p2[4].rstrip(),
+            'kolicina': int(float(p1[14])),
+            'nabavna_cijena': float(p1[18]),
+            'marza': float(p1[16]),
+            'cijena': float(p1[9]),
+            'rabat': float(p1[15])
+        }
 
-    return products
+        cachepath = f"{bazacachedir}/{product['sifra']}"
+        if os.path.exists(cachepath):
+            with open(cachepath) as cf:
+                cachedproduct = json.load(cf)
+                if cachedproduct == product:
+                    continue
+                else:
+                    print(product['sifra'])
+                    print(set(cachedproduct.items()) ^ set(product.items()))
 
-
-products = parseproducts(f'{bazatmp}/malmat.csv', f'{bazatmp}/malst.csv')
-print(products)
+        with open(cachepath, 'w') as cf:
+            json.dump(product, cf)
