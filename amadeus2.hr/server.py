0cf2f411fbab9dc74b1b4d10ffb4b3d9aa87ba9f
Bartol Deak <b@bdeak.net>
2020-12-22 00:08:24 +0100

    amadeus2.hr flask init

---
+++ amadeus2.hr/server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/kategorija/<category>')
+def category(category):
+	return f'kategorija: {category}'
+
+@app.route('/proizvod/<product>')
+def product(product):
+	return f'proizvod: {product}'
+
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+	return 'checkout'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	return 'login'
+
+@app.route('/orders', methods=['GET', 'POST', 'DELETE'])
+def orders():
+	return 'orders'
+
+app.run(debug=True)
