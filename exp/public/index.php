dc87fd1fddfb50665d3d2d520885ff9498eeba36
Bartol Deak <b@bdeak.net>
2021-03-10 23:08:09 +0100

    split code in more files

---
+++ exp/public/index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<?php require "../partials/head.php"; ?>
+	</head>
+	<body>
+		index
+		<?php require "../partials/mobile_nav.php"; ?>
+	</body>
+</html>
---
+++ exp/public/kategorije.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<?php require "../partials/head.php"; ?>
+	</head>
+	<body>
+		<div class="categories">
+			<div class="category">
+				<img src="tv.png" class="category_img">
+				<a href="/trazilica.php?kategorije[]=2" class="category_link dark_overlay">Televizije</a>
+			</div>
+		</div>
+		<?php require "../partials/mobile_nav.php"; ?>
+	</body>
+</html>
