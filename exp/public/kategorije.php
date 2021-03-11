97a7170950f106f068b47ee63029a48e95df66f8
Bartol Deak <b@bdeak.net>
2021-03-11 18:33:58 +0100

    update exp php

--- exp/public/kategorije.php
+++ exp/public/kategorije.php
@@ -3,10 +3,37 @@
 		<?php require "../partials/head.php"; ?>
 	</head>
 	<body>
-		<div class="categories">
+		<h1>Brendovi</h1>
+		<div class="brand_grid">
+			<div class="brand">
+				<a href="/trazilica.php?brendovi[]=1" class="brand_link">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 842 1000"><path d="M702 960c-54 53-114 44-171 20-61-26-116-27-180 0-80 34-122 24-170-20-271-279-231-704 77-720 75 4 127 41 171 44 65-13 128-51 198-46 84 7 147 40 189 100a220 220 0 0027 396c-32 83-73 166-141 227zM423 237C415 113 515 11 631 1c16 143-130 250-208 236z"/></svg>
+				</a>
+			</div>
+			<div class="brand">
+				<a href="/trazilica.php?brendovi[]=1" class="brand_link">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M27 27l-1-1V6l1-1h5v21l-1 1h-4zm-9 0V14c0-2-1-3-3-3H5v16H0V6 5h18c3 0 5 3 5 6v16h-5zm-9 0V14h5v13H9z"/></svg>
+				</a>
+			</div>
+			<div class="brand">
+				<a href="/trazilica.php?brendovi[]=1" class="brand_link">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 599.9 87.2"><path d="M100 32c-2 6-16 13-50 5l4 20c35 6 55 1 67-11 8-9 10-21 3-30-8-9-24-16-45-16-30 0-51 9-63 41L0 85h28l16-44c6-19 20-22 35-22 16 0 23 7 21 13zm275 19l-1-1a53 53 0 0137-1c1 1 2 2 1 3-1 4-26 3-37-1zm51-10c-6-4-17-7-28-7-22-1-47 7-52 24-3 10 7 24 33 28 13 2 33 2 48-3l5-15c-29 10-68 7-67-8 14 9 35 8 51 5 27-4 27-16 10-24zm46 10l-1-1a53 53 0 0136-1l2 3c-2 4-27 3-37-1zm51-10c-7-4-18-7-28-7-22-1-47 7-52 24-3 10 7 24 32 28 13 2 34 2 48-3l6-15c-30 10-69 7-68-8 15 9 36 8 51 5 27-4 27-16 11-24zm20 13l-11 31h25l9-24c2-6 4-11 11-11h18l5-14h-33c-11 0-20 5-24 18zM117 85h26l18-49h-27zm171-27c2-6 9-9 16-9 6 0 12 3 10 10l-9 26h25l10-26c5-14-6-25-30-25-27 0-42 8-48 24l-9 27h25l10-27zm-79-9c10 1 19 6 17 12-1 7-11 11-22 11-10 0-18-6-17-12 2-7 12-11 22-11zm4-15c-27-1-50 10-54 25-3 14 15 27 42 28 26 1 50-10 53-25 3-14-15-27-41-28z"/></svg>
+				</a>
+			</div>
+		</div>
+		<h1>Kategorije</h1>
+		<div class="category_grid">
 			<div class="category">
-				<img src="tv.png" class="category_img">
+				<img src="/demoimg/tv.png" class="category_img">
 				<a href="/trazilica.php?kategorije[]=2" class="category_link dark_overlay">Televizije</a>
+			</div>
+			<div class="category">
+				<img src="/demoimg/ps.png" class="category_img">
+				<a href="/trazilica.php?kategorije[]=2" class="category_link dark_overlay">Gaming</a>
+			</div>
+			<div class="category">
+				<img src="/demoimg/mob.png" class="category_img">
+				<a href="/trazilica.php?kategorije[]=2" class="category_link dark_overlay">Mobiteli</a>
 			</div>
 		</div>
 		<?php require "../partials/mobile_nav.php"; ?>
